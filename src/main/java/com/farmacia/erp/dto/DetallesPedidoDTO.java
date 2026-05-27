8b82b92e6f0a7c7f89581368ff588b1592a5302c
CeliaVillena04 <celiavillena04@uma.es>
2026-05-27 19:19:27 +0200

    Modficaciones

--- src/main/java/com/farmacia/erp/dto/DetallesPedidoDTO.java
+++ src/main/java/com/farmacia/erp/dto/DetallesPedidoDTO.java
@@ -1,5 +1,11 @@
-package com.farmacia.erp.DTO;
+package com.farmacia.erp.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DetallesPedidoDTO {
     private String idMedicamento;
     private Integer cantidad;
